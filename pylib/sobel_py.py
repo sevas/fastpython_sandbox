1d1e5bd36e5ecc9b5a4795a53ec3b8f79949f741
Frederic <f.degroef@gmail.com>
2023-07-31 15:35:02 +0200

    ENH: add cythonization of pure python file, for comparison

---
+++ pylib/sobel_py.py
@@ -0,0 +1,35 @@
+
+import numpy as np
+import math as m
+
+k_x = np.array([
+    [-1, 0, 1],
+    [-2, 0, 2],
+    [-1, 0, 1]])
+
+
+k_y = np.array([
+    [-1, -2, -1],
+    [ 0, 0, 0],
+    [ 1, 2, 1]])
+
+neighbour_idx = np.array([
+    [(-1, -1), (-1, 0), (-1, 1)],
+    [( 0, -1), ( 0, 0), ( 0, 1)],
+    [( 1, -1), ( 1, 0), ( 1, 1)],
+])
+
+
+def sobel(im, out):
+    h, w = im.shape
+    for y in range(1, h-1):
+        for x in range(1, w-1):
+            x_val = 0
+            y_val = 0
+            for yy in range(3):
+                for xx in range(3):
+                    dx, dy = neighbour_idx[yy, xx]
+                    x_val += k_x[yy, xx] * im[y+dy, x+dx]
+                    y_val += k_y[yy, xx] * im[y+dy, x+dx]
+
+            out[y, x] = m.sqrt(x_val*x_val + y_val*y_val)
--- setup.py
+++ setup.py
@@ -8,8 +8,10 @@ setup(
     version="0.0.1",
     ext_modules=cythonize([
         "pylib/math_cy.pyx",
-        # "pylib/math.py",
-        "pylib/sobel_cy.pyx"
+        # "pylib/mynumeric.py",
+        "pylib/sobel_cy.pyx",
+        "pylib/sobel.py"
+
     ], annotate=True, language_level=3),
     include_dirs=[np.get_include()]
 )
